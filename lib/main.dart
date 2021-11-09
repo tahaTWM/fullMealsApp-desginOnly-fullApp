e41f3327c48e6d7998f6e362b7b92d3ec5fd2df3
tahaTWM <taha.twm.1234@gmail.com>
2021-11-09 15:30:57 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:readmore/readmore.dart';
+import 'package:getwidget/getwidget.dart';
 
-void main() => runApp(new MaterialApp(home: MyApp()));
+void main() => runApp(new MaterialApp(
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class MyApp extends StatefulWidget {
   @override
@@ -15,181 +19,315 @@ class _MyApp extends State<MyApp> {
   Widget build(BuildContext context) {
     return new Material(
       child: new Container(
-          color: Colors.white,
-          child: new Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 40),
-            ),
+        child: new Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 40)),
             searchbox(),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-            ),
+            Padding(padding: EdgeInsets.only(top: 20)),
             mytitle(),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-            ),
+            Padding(padding: EdgeInsets.only(top: 7)),
             maelList(),
-            Padding(
-              padding: EdgeInsets.only(top: 15),
+            Padding(padding: EdgeInsets.only(top: 7)),
+            cardScroll2(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchbox() {
+    return new SizedBox(
+        width: 360,
+        height: 55,
+        child: new Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(50),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 12.0, // has the effect of softening the shadow
+                    spreadRadius: 1.0,
+                    offset: Offset(
+                      3.0, // horizontal, move right 10
+                      3.0, // vertical, move down 10
+                    ))
+              ]),
+          child: new Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+              ),
+              new Icon(
+                Icons.search,
+                size: 35,
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+              ),
+              Expanded(
+                  child: new TextField(
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  hintText: "Search for recipes and channals",
+                  hintStyle: TextStyle(fontSize: 16),
+                  border: InputBorder.none,
+                ),
+              ))
+            ],
+          ),
+        ));
+  }
+
+  Widget mytitle() {
+    return new SizedBox(
+        width: 340,
+        child: new Container(
+            decoration: BoxDecoration(
+                // color: Colors.grey,
+                border: Border(
+                    left: BorderSide(width: 10, color: Colors.greenAccent))),
+            child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(
+                    "   Pouplar Meals",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 5),
+                  ),
+                  new Text(
+                    "    This Week",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                  )
+                ])));
+  }
+
+  Widget cardScroll(String foodName, String imagePath) {
+    return new Container(
+        padding: EdgeInsets.all(5),
+        // decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.circular(50), color: Colors.purple),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
+          elevation: 8,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 5, 10, 5),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                ),
+                new CircleAvatar(
+                    backgroundImage: AssetImage(imagePath), radius: 40),
+                Padding(
+                  padding: EdgeInsets.only(left: 14),
+                ),
+                new Text(
+                  foodName,
+                  style: TextStyle(fontSize: 25),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add_shopping_cart_rounded,
+                    color: Colors.grey,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 5),
+                ),
+              ],
             ),
-            cardScroll2()
-          ])),
+          ),
+        ));
+  }
+
+  Widget maelList() {
+    return new Container(
+        width: 400,
+        height: 100,
+        child: new ListView(
+          scrollDirection: Axis.horizontal,
+          physics: BouncingScrollPhysics(),
+          children: <Widget>[
+            cardScroll("Cardamom Maple Salmon", "asset/Images/image1.jpg"),
+            cardScroll("Al's Burmese Chicken Curry", "asset/Images/image2.jpg"),
+            cardScroll("Sweet Potato and Venison Shepherd's Pie",
+                "asset/Images/image3.jpg"),
+            cardScroll(
+                "Beef Stroganoff with Ground Beef", "asset/Images/image1.jpg"),
+          ],
+        ));
+  }
+
+  Widget cardScroll2(BuildContext context) {
+    return new Expanded(
+      child: new ListView(
+        scrollDirection: Axis.vertical,
+        physics: BouncingScrollPhysics(),
+        children: <Widget>[
+          bottomImageScroll("asset/Images/image3.jpg", "Cardamom Maple Salmon",
+              "Roasting the pumpkin brings out the sweetness, which pairs well with the salty pancetta. This \"primo piatto,\" or first course, is simple and delicious!"),
+          new SizedBox(height: 15),
+          bottomImageScroll(
+              "asset/Images/image2.jpg",
+              "Al's Burmese Chicken Curry",
+              "Ground beef makes this stroganoff sauce a quick, tasty comfort food served over noodles or mashed spuds."),
+          bottomImageScroll(
+              "asset/Images/image3.jpg",
+              "Sweet Potato and Venison Shepherd's Pie",
+              "I tried to make a copycat Popeyes® fried chicken sandwich and ended up with a hybrid between that and the one from Chick-fil-A®. So you get the best of both worlds and what's more, this DIY version never runs out, not even on Sundays."),
+          new SizedBox(height: 15),
+          bottomImageScroll(
+              "asset/Images/image1.jpg",
+              "Beef Stroganoff with Ground Beef",
+              "These are such amazing Instant Pot® meatballs that will melt in your mouth the good way! Serve over noodles, rice, or on a sandwich roll!"),
+        ],
+      ),
+    );
+  }
+
+  Widget listview() {
+    return Container(
+      height: 430,
+      width: 430,
+      color: Colors.amber,
+      // child: SingleChildScrollView(
+      //   scrollDirection: Axis.horizontal,
+      //   child: Row(
+      //     children: [
+      //       lastItems(context, "asset/Images/image3.jpg", "title first",
+      //           "subtitle first", "content first"),
+      //       lastItems(context, "asset/Images/image1.jpg", "title Second",
+      //           "subtitle Second", "content Second"),
+      //       lastItems(context, "asset/Images/image2.jpg", "title Third",
+      //           "subtitle Third", "content Third"),
+      //     ],
+      //   ),
+      // ),
+    );
+  }
+
+  Widget bottomImageScroll(String path, String text1, String text2) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 4,
+                child: Text(
+                  text1,
+                  style: TextStyle(fontSize: 22),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add_shopping_cart_rounded,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          ReadMoreText(
+            text2,
+            style: TextStyle(
+              fontSize: 15,
+            ),
+            trimLines: 2,
+            trimMode: TrimMode.Line,
+            trimCollapsedText: 'show more',
+            trimExpandedText: 'show less',
+            moreStyle: TextStyle(
+              color: Colors.blue,
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+            ),
+            lessStyle: TextStyle(
+              color: Colors.blue,
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              image: DecorationImage(
+                image: AssetImage(path),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget lastItems(BuildContext context, String imagepath, String title,
+      String subtitle, String content) {
+    return GFCard(
+      elevation: 13,
+      padding: EdgeInsets.zero,
+      color: Colors.red,
+      boxFit: BoxFit.cover,
+      titlePosition: GFPosition.start,
+      image: Image.asset(
+        imagepath,
+        fit: BoxFit.cover,
+      ),
+      showImage: true,
+      title: GFListTile(
+        avatar: GFAvatar(
+          backgroundImage: AssetImage(imagepath),
+        ),
+        titleText: title,
+        subTitleText: subtitle,
+      ),
+      content: Text(
+        content,
+      ),
+      buttonBar: GFButtonBar(
+        children: <Widget>[
+          GFAvatar(
+            child: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+          ),
+          GFAvatar(
+            backgroundColor: GFColors.DARK,
+            child: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+          ),
+          GFAvatar(
+            backgroundColor: GFColors.DANGER,
+            child: Icon(
+              Icons.help,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-Widget searchbox() {
-  return new SizedBox(
-      width: 360,
-      height: 55,
-      child: new Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(50),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black54,
-                  blurRadius: 10.0, // has the effect of softening the shadow
-                  spreadRadius: 1.0,
-                  offset: Offset(
-                    10.0, // horizontal, move right 10
-                    10.0, // vertical, move down 10
-                  ))
-            ]),
-        child: new Row(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 10),
-            ),
-            new Icon(
-              Icons.search,
-              size: 35,
-            ),
-            Padding(
-              padding: EdgeInsets.only(right: 10),
-            ),
-            Expanded(
-                child: new TextField(
-              style: TextStyle(fontSize: 20),
-              decoration: InputDecoration(
-                hintText: "Search for recipes and channals",
-                hintStyle: TextStyle(fontSize: 16),
-                border: InputBorder.none,
-              ),
-            ))
-          ],
-        ),
-      ));
-}
-
-Widget mytitle() {
-  return new SizedBox(
-      width: 340,
-      child: new Container(
-          decoration: BoxDecoration(
-              // color: Colors.grey,
-              border: Border(
-                  left: BorderSide(width: 10, color: Colors.greenAccent))),
-          child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(
-                  "   Pouplar Meals",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 5),
-                ),
-                new Text(
-                  "    This Week",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                )
-              ])));
-}
-
-Widget cardScroll(String foodName, String imagePath) {
-  return new Container(
-      width: 250,
-      // decoration: BoxDecoration(
-      //     borderRadius: BorderRadius.circular(50), color: Colors.purple),
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
-        color: Colors.white,
-        child: Row(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 10),
-            ),
-            new CircleAvatar(
-                backgroundImage: AssetImage(imagePath), radius: 40),
-            Padding(
-              padding: EdgeInsets.only(left: 14),
-            ),
-            new Text(
-              foodName,
-              style: TextStyle(fontSize: 25),
-            )
-          ],
-        ),
-      ));
-}
-
-Widget maelList() {
-  return new Container(
-      width: 400,
-      height: 100,
-      child: new ListView(
-        scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          cardScroll("taha", "Assest/Images/logo2.jpg"),
-          cardScroll("TWM", "Assest/Images/logo2.jpg"),
-          cardScroll("taha", "Assest/Images/logo2.jpg"),
-          cardScroll("TWM", "Assest/Images/logo2.jpg"),
-        ],
-      ));
-}
-
-Widget cardScroll2() {
-  return new Container(
-    // decoration: BoxDecoration(color: Colors.purple),
-    width: 400,
-    height: 430,
-    child: new ListView(
-      scrollDirection: Axis.vertical,
-      children: <Widget>[
-        bottomImageScroll("Assest/Images/foodDetials.jpg", "taha", "TWM"),
-        new SizedBox(height: 15),
-        bottomImageScroll("Assest/Images/logo2.jpg", "taha", "TWM"),
-        bottomImageScroll("Assest/Images/foodDetials.jpg", "taha", "TWM"),
-        new SizedBox(height: 15),
-        bottomImageScroll("Assest/Images/logo2.jpg", "taha", "TWM"),
-      ],
-    ),
-  );
-}
-
-Widget bottomImageScroll(String path, String text1, String text2) {
-  return new Card(
-    child: new Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        new Text(
-          text1,
-          style: TextStyle(fontSize: 25),
-        ),
-        new Text(
-          text2,
-          style: TextStyle(fontSize: 30),
-        ),
-        Image(image: AssetImage(path), width: 400, fit: BoxFit.fill)
-      ],
-    ),
-  );
-}
-
-
-
-
 
 
 
